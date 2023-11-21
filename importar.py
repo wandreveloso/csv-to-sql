adec1332133e119665e3ed2897929bd4a5af1818
wandreveloso <wandreveloso@gmail.com>
2023-11-21 20:55:20 -0300

    translate the comments to english

--- importar.py
+++ importar.py
@@ -1,22 +1,22 @@
 import csv
 
-# Função para limpar valores nulos
+# Function to clean null values
 def clean_value(value):
     return value.strip() if value else None
 
-# Função para formatar o valor para o comando SQL
+# Function to format a value to SQL command
 def _format_sql_value(value):
     return 'NULL' if value is None else f"'{value}'"
 
-# Caminho do arquivo CSV
+# Path of CSV file
 csv_file_path = 'novos.csv'
 
-# Leitura do cabeçalho do arquivo CSV
+# Reading the header of CSV file
 with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file)
-    header = next(reader)  # Ignora o cabeçalho
+    header = next(reader)  # Header ignore
 
-# Índices dos atributos no cabeçalho
+# Indexes of header attributes
 tb_collection_name_idx = next((i for i, h in enumerate(header) if 'tb_collection.name' in h), None)
 tb_place_shelf_idx = next((i for i, h in enumerate(header) if 'tb_place.shelf' in h), None)
 tb_book_name_idx = next((i for i, h in enumerate(header) if 'tb_book.name' in h), None)
@@ -35,16 +35,16 @@ tb_gender_name_idx = next((i for i, h in enumerate(header) if 'tb_gender.name' i
 tb_book_isbn_idx = next((i for i, h in enumerate(header) if 'tb_book.ISBN' in h), None)
 tb_book_pages_idx = next((i for i, h in enumerate(header) if 'tb_book.pages' in h), None)
 
-# Listas para armazenar valores únicos
+# Lists to store unique values
 collections = set()
 genders = set()
 publishers = set()
 persons = set()
 places = set()
 
-# Gera comandos INSERT INTO
+# Generate commands INSERT INTO
 with open('insert_commands.sql', 'w', encoding='utf-8') as sql_file:
-    # Inserção nas tabelas intermediárias
+    # Insertion on intermediate tables
     for collection_name in collections:
         sql_file.write(f"INSERT INTO tb_collection (name) VALUES ('{collection_name}');\n")
 
@@ -60,13 +60,13 @@ with open('insert_commands.sql', 'w', encoding='utf-8') as sql_file:
     for place_shelf in places:
         sql_file.write(f"INSERT INTO tb_place (shelf) VALUES ('{place_shelf}');\n")
 
-    # Inserção na tabela principal (tb_book)
+    # Insertion on main table (tb_book)
     with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
-        header = next(reader)  # Ignora o cabeçalho novamente
+        header = next(reader)  # Header ignore again
 
         for row in reader:
-            # Extrai os valores da linha
+            # Extract the line values
             collection_name = clean_value(row[tb_collection_name_idx])
             place_shelf = clean_value(row[tb_place_shelf_idx])
             book_name = clean_value(row[tb_book_name_idx])
@@ -85,12 +85,12 @@ with open('insert_commands.sql', 'w', encoding='utf-8') as sql_file:
             book_isbn = clean_value(row[tb_book_isbn_idx])
             book_pages = clean_value(row[tb_book_pages_idx])
 
-            # Verifica se a tabela tb_place (shelf) já foi inserida
+            # Verify if the table tb_place (shelf) already was inserted
             if place_shelf and place_shelf not in places:
                 sql_file.write(f"INSERT INTO tb_place (shelf) VALUES ('{place_shelf}');\n")
                 places.add(place_shelf)
 
-            # Gera o comando INSERT INTO para a tabela principal (tb_book)
+            # Generate the command INSERT INTO to main table (tb_book)
             sql_file.write(
                 f"INSERT INTO tb_book (name, short_description, description, book_size, book_year, sale, "
                 f"city, book_condition, edition, ISBN, pages, placeid) VALUES "
@@ -101,7 +101,7 @@ with open('insert_commands.sql', 'w', encoding='utf-8') as sql_file:
                 f"(SELECT placeid FROM tb_place WHERE shelf = {_format_sql_value(place_shelf)} LIMIT 1));\n"
             )
 
-            # Obtém os IDs correspondentes das tabelas intermediárias
+            # Get the  IDs of intermediate tables
             collection_id_query = f"SELECT collectionid FROM tb_collection WHERE name = '{collection_name}' LIMIT 1"
             book_id_query = f"SELECT bookid FROM tb_book WHERE name = '{book_name}' LIMIT 1"
             person_author_id_query = f"SELECT personid FROM tb_person WHERE name = '{person_name_author}' LIMIT 1"
@@ -110,7 +110,7 @@ with open('insert_commands.sql', 'w', encoding='utf-8') as sql_file:
             publisher_id_query = f"SELECT publisherid FROM tb_publisher WHERE name = '{publisher_name}' LIMIT 1"
             place_id_query = f"SELECT placeid FROM tb_place WHERE shelf = '{place_shelf}' LIMIT 1"
 
-            # Gera comandos INSERT INTO para tabelas intermediárias
+            # Generate commands INSERT INTO to intermediate tables
             if collection_name:
                 sql_file.write(
                     f"INSERT IGNORE INTO tb_collection_has_tb_book (collectionid, bookid) VALUES (({collection_id_query}), ({book_id_query}));\n"
